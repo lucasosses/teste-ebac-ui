8a2ae331347708747c32e11e2d3d4b4d634134d2
lucasosses <lucasosses.qa@gmail.com>
2025-05-12 19:18:35 -0300

    aula-page-object

--- cypress/e2e/3-loja-ebac/3-produtos.cy.js
+++ cypress/e2e/3-loja-ebac/3-produtos.cy.js
@@ -1,33 +1,30 @@
 /// <reference  types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
-    });
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            .first()
-            .click()
-        cy.get('.widget-title > span').should('exist')
-    })
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            .last()
-            .click()
-        cy.get('.widget-title > span').should('exist')
-    })
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            .eq(2)
-            .click()
-        cy.get('.widget-title > span').should('exist')
-    })
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            .contains('Aero Daily Fitness Tee')
-            .click()
-        cy.get('.widget-title > span').should('exist')
+        produtosPage.visitarUrl()
     });
     
+    it('Deve selecionar um produto da lista', () => {
+        produtosPage.buscarProdutoLista ('Arcadio Gym Short')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        
+    })
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Ajax Full-Zip Sweatshirt'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+        
+    });
+
+    it('Deve visitar a página do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
+    });
 });
---
+++ cypress/support/page-objects/produtos.page.js
@@ -0,0 +1,28 @@
+class produtosPage {
+
+visitarUrl() {
+    cy.visit('produtos')
+}
+
+buscarProduto(nomeProduto) {
+   cy.get('[name="s"]').eq(1).type(nomeProduto)
+   cy.get('.button-search').eq(1).click()
+}
+
+buscarProdutoLista(nomeProduto) {
+ cy.get('.products > .row')
+    .contains(nomeProduto)
+    .click()
+}
+
+visitarProduto() {
+
+}
+
+addProdutoCarrinho() {
+
+}
+
+}
+
+export default new produtosPage()
